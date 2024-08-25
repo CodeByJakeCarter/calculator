dcc62e363155312d55dbe67cfb794c8cd76566d1
Jake Carter <[80532104+D-Monkey@users.noreply.github.com]>
2024-08-25 20:02:49 +0300

    add division function to backend

--- backend.js
+++ backend.js
@@ -6,7 +6,7 @@ const subtract = function (...nums) {
   return nums.reduce((total, currentItem) => total - currentItem, 2 * nums[0]);
 };
 
-const multiply = function (nums) {
+const multiply = function (...nums) {
   return nums.reduce((total, currentItem) => total * currentItem, 1);
 };
 
@@ -16,4 +16,8 @@ const power = function (num1, num2) {
     array.push(num1);
   }
   return array.reduce((total, currentItem) => total * currentItem, 1);
+};
+
+const divide = function (...nums) {
+  return nums.reduce((total, currentItem) => total / currentItem);
 };
