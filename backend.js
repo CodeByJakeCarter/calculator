0ee6de758c8a85b8f52f021f491badf74e13d761
Jake Carter <[80532104+D-Monkey@users.noreply.github.com]>
2024-08-25 19:57:03 +0300

    add subtraction function to backend

--- backend.js
+++ backend.js
@@ -1,3 +1,7 @@
 const add = function (...nums) {
   return nums.reduce((total, currentItem) => total + currentItem, 0);
 };
+
+const subtract = function (...nums) {
+  return nums.reduce((total, currentItem) => total - currentItem, 2 * nums[0]);
+};
