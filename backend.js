bc52816aa77f89d0c9574026836005eceaccc080
Jake Carter <[80532104+D-Monkey@users.noreply.github.com]>
2024-08-25 20:45:26 +0300

    add operate function, boilerplate, container div

--- backend.js
+++ backend.js
@@ -21,3 +21,20 @@ const power = function (num1, num2) {
 const divide = function (...nums) {
   return nums.reduce((total, currentItem) => total / currentItem);
 };
+
+const primary = "";
+const secondary = "";
+const operator = "";
+
+const operate = function (primary, secondary, operator) {
+  switch (operator) {
+    case "*":
+      return multiply(primary, secondary);
+    case "+":
+      return add(primary, secondary);
+    case "/":
+      return divide(primary, secondary);
+    case "-":
+      return subtract(primary, secondary);
+  }
+};
